3678429146327f8e7d95156a8db8d01d73520006
aleangelozi <aleangelozi@gmail.com>
2021-04-28 22:10:56 -0300

    Replacing findViewById by View Binding.

--- app/src/main/java/com/technibyte/autocompletetextview_kotlin/MainActivity.kt
+++ app/src/main/java/com/technibyte/autocompletetextview_kotlin/MainActivity.kt
@@ -2,19 +2,24 @@ package com.technibyte.autocompletetextview_kotlin
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
+import com.technibyte.autocompletetextview_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val suggestions = resources.getStringArray(R.array.Cities)
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, suggestions)
 
-        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        val autoCompleteTextView = binding.autoCompleteTextView
         autoCompleteTextView.threshold = 0
         autoCompleteTextView.setAdapter(arrayAdapter)
 
